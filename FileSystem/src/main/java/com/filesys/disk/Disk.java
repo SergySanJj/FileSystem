aa9880bdb6e696786d15320a8dd51ccef21ea4d3
Sergei <isara.isara8@gmail.com>
2020-06-07 22:42:35 +0300

    Add disk representation

---
+++ FileSystem/src/main/java/com/filesys/disk/Disk.java
@@ -0,0 +1,53 @@
+package com.filesys.disk;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class Disk {
+    public static final int diskCylindersCount = 4;
+    public Cylinder[] cylinders;
+
+    public Disk() {
+        cylinders = new Cylinder[diskCylindersCount];
+        for (int i = 0; i < cylinders.length; i++) cylinders[i] = new Cylinder();
+    }
+
+    public int size() {
+        return diskCylindersCount * cylinders[0].size();
+    }
+
+
+    public class Cylinder {
+        public static final int cylinderSize = 4;
+        public Track[] tracks;
+
+        public Cylinder() {
+            tracks = new Track[cylinderSize];
+            for (int i = 0; i < cylinders.length; i++) tracks[i] = new Track();
+        }
+
+        public int size() {
+            return cylinderSize * tracks[0].size();
+        }
+
+        public class Track {
+            public static final int trackSize = 8;
+            public Sector[] sectors;
+
+            public Track() {
+                sectors = new Sector[trackSize];
+                for (int i = 0; i < cylinders.length; i++) sectors[i] = new Sector();
+            }
+
+            public int size() {
+                return sectors.length * Sector.sectorSize;
+            }
+
+            public class Sector {
+                public static final int sectorSize = 64;
+                public byte[] bytes = new byte[sectorSize];
+            }
+        }
+    }
+
+
+}
